70abef1fa1dbca1802424ace1eda6c5f4aa5142d
consultati <eazurara@gmail.com>
2020-11-22 14:23:32 -0300

    Firebase Setup
    Setting up Firebase Database

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,6 +1,24 @@
-// import something here
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
 
-// "async" is optional
-export default async ({ /* app, router, Vue, ... */ }) => {
-  // something to do
-}
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+// import "firebase/firestore";
+
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+  apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
+  authDomain: "myapp-project-123.firebaseapp.com",
+  databaseURL: "https://myapp-project-123.firebaseio.com",
+  projectId: "myapp-project-123",
+  storageBucket: "myapp-project-123.appspot.com",
+  messagingSenderId: "65211879809",
+  appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
+  measurementId: "G-8GSGZQ44ST"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
