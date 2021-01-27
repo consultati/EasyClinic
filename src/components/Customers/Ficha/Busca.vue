583ae01d4e637836f6cf189f5340f17e5f7e38ba
consultati <eazurara@gmail.com>
2021-01-27 19:14:44 -0300

    Update Busca.vue

--- src/components/Customers/Ficha/Busca.vue
+++ src/components/Customers/Ficha/Busca.vue
@@ -1,35 +1,75 @@
 <template>
     <div >
       <div class="text-h5 tx-italic-bold q-mb-md">Busca de Informações do Paciente</div>
-        <q-select rounded outlined bottom-slots v-model="model" :options="options" label="Nome do Paciente" counter maxlength="12" :dense="dense" :options-dense="denseOpts">
+         <q-input bottom-slots v-model="text" label="Nome do Paciente" counter maxlength="25" :dense="dense">
         <template v-slot:before>
           <q-icon name="account_box" />
         </template>
 
         <template v-slot:append>
-          <q-icon v-if="model !== ''" name="close" @click.stop="model = ''" class="cursor-pointer" />
-          <q-icon name="search" @click.stop />
+          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
+          <q-icon name="search" />
         </template>
 
         <template v-slot:hint>
-          Digite o nome do paciente
+          <!-- Digite o nome do Paciente -->
         </template>
-      </q-select>
+      </q-input>
+
+      <q-card class="row" flat bordered v-for="(item, index) in pacientes" :key="index">
+        <q-card-section class="col" v-html="item.nome" />
+        <q-btn flat color="primary" @click="editar(index, item.id)">Editar</q-btn>
+        <q-btn flat color="red" @click="eliminar(index, item.id)">Eliminar</q-btn>
+      </q-card>  
+
     </div>    
 </template>
 
 <script>
+import { db } from "boot/firebase";
 export default {
-      data () {
+  data () {
     return {
       model: null,
-
-      options: [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-      ],
-
+      text: '',
+      pacientes: [],
       dense: false,
-      denseOpts: false
+      denseOpts: false,
+      id: null,
+      index: null
+    }
+  },
+
+  created() {
+    this.listarClientes();
+  },
+
+  methods: {
+    async listarClientes(){
+      try {
+
+        const resDB = await db.collection('clientes').get()
+
+        resDB.forEach(element => {
+          console.log(element.id);
+          const cliente = {
+            id: element.id,
+            nome: element.data().cliName, 
+            cpf: element.data().cliCPF        
+          }
+          this.pacientes.push(cliente);
+          console.log(this.pacientes);
+        });
+
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    editar(index, id){
+      console.log('EDITAR');
+    },
+    eliminar(index, id){
+      console.log('ELIMINAR');
     }
   }
 }
